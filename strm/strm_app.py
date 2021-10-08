d76a99213951476ab6120b698092e3748581b9e6
Nikhil <61196335+nik-cy-dv@users.noreply.github.com>
2021-10-09 00:48:36 +0530

    Update strm_app.py

--- strm/strm_app.py
+++ strm/strm_app.py
@@ -19,7 +19,7 @@ import datetime
 import tensorflow as tf
 from tensorflow import keras 
 from tensorflow.keras import preprocessing
-from tensorflow.keras.models import load_model, load_weights
+from tensorflow.keras.models import load_model #, load_weights
 #import keras
 
 
@@ -152,8 +152,8 @@ def feature_engg(data, col1, col2, col3, col4, col5):
 def model_pred(frame):
   
     
-  #best_model = tf.keras.models.load_model(path) 
-  best_model  = model.load_weights(path)
+  best_model = tf.keras.models.load_model(path) 
+  #best_model  = model.load_weights(path)
   if uploaded_file is not None:
     # User-selected image.
     content = Image.open(uploaded_file)
