012a2b1935b9259710ee7275365802e04b328e71
Nikhil <61196335+nik-cy-dv@users.noreply.github.com>
2021-10-08 23:30:29 +0530

    Update strm_app.py

--- strm/strm_app.py
+++ strm/strm_app.py
@@ -100,8 +100,7 @@ ohe = pickle.load(open('pickle/ohe.pkl', 'rb'))
 feature_labels = pickle.load(open('pickle/labels.pkl', 'rb'))
 scaler = pickle.load(open('pickle/scaler.pkl', 'rb'))
 gbdt = pickle.load(open('pickle/imgonly_gbr_model.pkl', 'rb'))
-path = 'blob/main/model3_deepl'#'cs-2-deploy/blob/main/model3_deepl.h5'
-#path = 'D:/data applied ai/ASSIGNMENTS/case study 2/pickle/model3_img_only.hdf5'
+path = 'model3_deepl.h5'#'blob/main/model3_deepl.h5'#'cs-2-deploy/blob/main/model3_deepl.h5'
 
 
 def feature_engg(data, col1, col2, col3, col4, col5):
